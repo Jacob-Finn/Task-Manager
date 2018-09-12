7e1dd23810fc6b0c2e4ecbe49c618d886390bb76
JacobFinn <Jacob.Finn102@gmail.com>
2018-09-12 09:59:41 -0500

    Initial Commit

---
+++ TaskManager/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TaskManager
+//
+//  Created by Jacob Finn on 9/12/18.
+//  Copyright © 2018 Jacob Finn. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
